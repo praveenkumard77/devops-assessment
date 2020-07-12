9fdbaf990327800cd624f03b4c4a5365f16fdecf
praveenkumard77 <58468974+praveenkumard77@users.noreply.github.com>
2020-07-13 02:55:04 +0530

    update resource

--- 1b./resource.tf
+++ 1b./resource.tf
@@ -1,11 +1,5 @@
-provider "aws" {
-  region     = "${var.AWS_REGION}"
-  access_key = "${var.AWS_ACCESS_KEY}"
-  secret_key = "${var.AWS_SECRET_KEY}"
-}
-
 resource "aws_vpc" "assess" {
-  cidr_block       = "${var.vpc_cidr}"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
   tags = {
@@ -14,12 +8,13 @@ resource "aws_vpc" "assess" {
 }
 
 resource "aws_instance" "assess-instance" {
-  count         = "${var.instance_count}"
-  ami           = "${lookup(var.ami,var.AWS_REGION)}"
-  instance_type = "${var.instance_type}"
+  count         = var.instance_count
+  ami           = lookup(var.ami,var.AWS_REGION)
+  instance_type = var.instance_type
+}
 
 resource "aws_vpn_gateway" "assess_vpn" {
-  vpc_id = "${aws_vpc.assess.id}"
+  vpc_id = aws_vpc.assess.id
 }
 
 resource "aws_customer_gateway" "assess_cust_gw" {
@@ -29,8 +24,8 @@ resource "aws_customer_gateway" "assess_cust_gw" {
 }
 
 resource "aws_vpn_connection" "assess_vpn_con" {
-  vpn_gateway_id      = "${aws_vpn_gateway.assess_vpn.id}"
-  customer_gateway_id = "${aws_customer_gateway.assess_cust_gw.id}"
+  vpn_gateway_id      = aws_vpn_gateway.assess_vpn.id
+  customer_gateway_id = aws_customer_gateway.assess_cust_gw.id
   type                = "ipsec.1"
   static_routes_only  = true
 }
